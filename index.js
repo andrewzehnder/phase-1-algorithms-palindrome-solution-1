b0efbc630bb5e21d503020cc1fec8ae6552c5e32
Andrew <azehnder2@gmail.com>
2022-05-06 15:24:51 -0700

    commit tree 000c614a64e559f5ae5413ed9edf9e780e86b8d7

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
 }
 
 /* 
-  Add your pseudocode here
+  Create a new function to reverse the word.
+  Split the word into an array in order to reverse it.
+  Once it's reverse, join it back together to be a word.
+  Return the reversed word.
+
+  Create a function to compare the reversed word to the word.
+  Since it's true or false, just return the result.
 */
 
 /*
-  Add written explanation of your solution here
+We need to check to see if the word is a palindrome. To do that, we need to reverse the word and compare it to the original word.
+If it is, return true. If it's not, return false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
